d0615f470b1c54c42476ad380fb2c9aa5e255e73
@Dmitry-Boyko <8184160900.cell@gmail.com>
2022-03-23 18:08:00 -0400

    apdated js

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const butnUpdate = document.getElementById('btn-main');
-butnUpdate.addEventListener('click', () => {
-    headline.style.border = 'solid 2px red';
-    headline.style.fontSize = '60px';
-});
-
-const headline = document.getElementById('headline');
