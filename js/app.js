5bca0e126fda86d3527a7df44721390cdbdd7687
@Dmitry-Boyko <8184160900.cell@gmail.com>
2022-03-23 18:13:25 -0400

    apdated js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,3 @@
-// const body = document.body
-// document.body.addEventListener('click', () => {
-//     console.log('You click the body')
-//     body.innerHTML = '<h1>Hello My New World!</h1>';
-// });
-
 const butnUpdate = document.getElementById('btn-main');
 butnUpdate.addEventListener('click', () => {
     headline.style.border = 'solid 2px red';
@@ -11,4 +5,3 @@ butnUpdate.addEventListener('click', () => {
 });
 
 const headline = document.getElementById('headline');
-//headline.style.border = 'solid 2px red';
