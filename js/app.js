6c75d6fec689d569b571cfbb9d683896098c57c5
@Dmitry-Boyko <8184160900.cell@gmail.com>
2022-03-23 18:10:52 -0400

    apdated js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 // const body = document.body
-
 // document.body.addEventListener('click', () => {
 //     console.log('You click the body')
 //     body.innerHTML = '<h1>Hello My New World!</h1>';
