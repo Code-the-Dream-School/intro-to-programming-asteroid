76cfd2eec03260068018efd018c7d96cd8f10d72
@Dmitry-Boyko <8184160900.cell@gmail.com>
2022-03-26 12:25:08 -0400

    work with code

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ const today = new Date();
 const thisYear = today.getFullYear();
 const footer = document.querySelector('footer');
 const copyright = document.createElement('p');
-
 copyright.innerHTML = (`&copy Dmitry Boyko ${thisYear}`);
 footer.appendChild(copyright);
 
