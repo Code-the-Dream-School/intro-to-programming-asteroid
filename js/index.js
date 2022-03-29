b77b666f7934d99ea2105a5efef365e85ce398d1
George Owusu <george@macbook-pro.myfiosgateway.com>
2022-03-29 06:03:34 -0400

    added index.js file and added to html

--- js/index.js
+++ js/index.js
@@ -11,8 +11,39 @@ const skillsSection = document.getElementById('skills');
 const skillsList = skillsSection.querySelector('ul');
 
 for(let i = 0; i < skills.length; i++){
-    skills[i];
     const skill = document.createElement('li');
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+
+const messageForm = document.getElementById('leaveMessage');
+
+messageForm.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    const name = e.target.name;
+    const email = e.target.email;
+    const message = e.target.message;
+    
+    console.log(name);
+    console.log(email);
+    console.log(message);
+
+    const messageSection = document.getElementById('message');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = '<span><a href="mailto:gkojunior@gmail.com">Email</a></span>';
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'remove';
+    input.type = 'button';
+
+    document.getElementById("leaveMessage").reset();
+});
+
+removeButton.addEventListener('click' , (e) => {
+    const entry = button.parentNode;
+    entry.remove();
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+});
