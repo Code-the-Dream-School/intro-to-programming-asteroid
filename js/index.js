783e450490deb5df35d3b675ba4ab26c6da69644
Nur <nuriyealp66@gmail.com>
2022-04-05 06:32:33 -0700

    add html form

--- js/index.js
+++ js/index.js
@@ -9,7 +9,36 @@ const skills = ['HTML','CSS','JavaScript','Git','GitHub'];
 const skillsSection = document.getElementById('skills');
 const skillList = skillsSection.querySelector('ul');
 for(let i=0; i<skills.length; i++){
-    let item = document.createElement('li')
-    item.textContent = skills[i]
+    let item = document.createElement('li');
+    item.textContent = skills[i];
     skillList.append(item);
 }
+
+const messageForm = document.getElementsByName("leave_message")[0];
+messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const message = event.target.message.value;
+    console.log(name,email,message);
+
+    const messageSection = document.getElementById("messages");
+    const messageList = messageSection.querySelector("ul");
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href="mailto:${email}">${name}</a><span> wrote : ${message}</span>`;
+
+    const removeButton = document.createElement('button');
+    removeButton.innerHTML ="remove";
+    removeButton.setAttribute('type' , 'button');
+
+    removeButton.addEventListener('click', (e) => {
+        const entry = newMessage.querySelector('button').parentNode;
+        entry.remove();
+    });
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+
+    messageForm.reset()
+});
