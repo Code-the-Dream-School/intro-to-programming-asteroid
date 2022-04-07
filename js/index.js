8454b29058a1c43cdf96aecc2b4b37aff63857e0
George Owusu <george@macbook-pro.myfiosgateway.com>
2022-04-07 00:04:13 -0400

    created a form, added message List Section, handled message form submit and displaying it in the message section

--- js/index.js
+++ js/index.js
@@ -11,8 +11,80 @@ const skillsSection = document.getElementById('skills');
 const skillsList = skillsSection.querySelector('ul');
 
 for(let i = 0; i < skills.length; i++){
-    skills[i];
     const skill = document.createElement('li');
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+// DOM Manipulation
+
+const form = document.getElementById('leaveMessage');
+const input = form.querySelector('input');
+const ul = document.getElementById('messageList');
+
+function createLI(text, email, message) {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    span.textContent = text + " (" + email + ") says: " + message;
+    li.appendChild(span);
+
+    const editButton = document.createElement('button');
+    editButton.textContent = 'edit';
+    li.appendChild(editButton);
+
+    return li;
+  }
+
+// Handle Message Form Submit
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = e.target.elements.submitterName.value;
+    const email = e.target.elements.email.value;
+    const messages = e.target.elements.submitterMessage.value;
+    console.log(name);
+    console.log(email);
+    console.log(messages);
+
+    const li = createLI(name, email, messages);
+    ul.appendChild(li);
+
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href="mailto: ${email}">${name}></a>`;
+    messageList.appendChild(newMessage);
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'remove';
+    removeButton.type = "button"
+    li.appendChild(removeButton);
+
+    form.reset();
+  });                                 
+  
+  ul.addEventListener('click', (e) => { // edit handler
+    if (e.target.tagName === 'BUTTON') {
+      const button = e.target;
+      const li = button.parentNode;
+      const ul = li.parentNode;
+      if (button.textContent === 'remove') {
+      ul.removeChild(li);
+      } else if (button.textContent === 'edit') {
+        const span = li.firstElementChild;
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.value = span.textContent;
+        li.insertBefore(input, span);
+        li.removeChild(span);
+        button.textContent= 'save';
+        } else if (button.textContent === 'save') {
+        const input = li.firstElementChild;
+        const span = document.createElement('span');
+        span.textContent = input.value;
+        li.insertBefore(span, input);
+        li.removeChild(input);
+        button.textContent = 'edit'; 
+      }
+    }
+  });
