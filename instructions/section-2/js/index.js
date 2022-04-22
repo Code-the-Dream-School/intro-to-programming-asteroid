6bf21f039dac47bdfc82bfdc6c5edb6998822fe4
Armanchi <89354548+Armanchi@users.noreply.github.com>
2022-04-22 14:48:43 -0500

    added text to index.css

--- instructions/section-2/js/index.js
+++ instructions/section-2/js/index.js
@@ -1,3 +1,4 @@
+
 const today = new Date();
 const thisYear = today.getFullYear();
 const footer = document.querySelector('footer');
@@ -14,7 +15,7 @@ for (i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 };
-
+//DOM
 const messageForm = document.getElementsByName("leave_message")[0];
 console.log(messageForm);
 messageForm.addEventListener('submit', (event) => {
@@ -47,3 +48,39 @@ messageForm.addEventListener('submit', (event) => {
     messageForm.reset();  
 });
 
+
+//intersection observer
+window.addEventListener('DOMContentLoaded', setup); 
+
+function setup() {
+    const options = {
+        rootMargin: '0px 0px -200px 0px'
+    }
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            } else {
+                return;
+            }
+        })
+    }, options);
+
+    //for h2s
+    const comeIn = document.querySelectorAll('h2');
+    comeIn.forEach(h2 => observer.observe(h2));
+    //for <p>
+    const paras = document.querySelectorAll('p');
+    paras.forEach(p => observer.observe(p));
+    //for form
+    const frm = document.querySelectorAll('form');
+    frm.forEach(form => observer.observe(form)); 
+    //for img
+    const image = document.querySelectorAll('img');
+    image.forEach(img => observer.observe(img));
+    //for span
+    const spans = document.querySelectorAll('span');
+    spans.forEach(span => observer.observe(span));
+}
