9e490591f1d52ed71b0cf276b6501bb4773205ae
George Owusu <georgeowusu@georges-mbp.myfiosgateway.com>
2022-05-08 16:19:39 -0400

    forked new assignment

--- instructions/section-6/js/portfolio.js
+++ instructions/section-6/js/portfolio.js
@@ -99,6 +99,32 @@ form.addEventListener('submit', (e) => {
     }
   });
 
+ // Fetch GitHub Repositories
+  
+  const githubRequest = new XMLHttpRequest();
+  githubRequest.onreadystatechange = function () {
+    if (githubRequest.readyState === 4 && githubRequest.status === 200) {     
+    githubRequest.responseText
+    }
+  };
+  githubRequest.open('GET', url);
+  githubRequest.send();
+
+
+  //Handle Response from Server
+  githubRequest.addEventListener('load', function () {
+    const repositories = JSON.parse(this.response);
+    console.log(repositories)
+    
+    const projectSection = document.getElementById('projects');
+    const projectList = projectSection.querySelector('ul');
+    //   Display Repositories in List
+    for (let i = 0; i <= repositories.length; i++) {
+      const project = document.createElement('li');
+      project.innerText = repositories[i].name;
+      projectList.appendChild(project);
+    }
+  });
 
 
   
