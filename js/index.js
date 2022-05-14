18b937c724c177ffd35a685bf3b353dcb460e510
jennyfertellez <102021487+jennyfertellez@users.noreply.github.com>
2022-05-14 00:38:27 -0700

    Updated skills and match font format

--- js/index.js
+++ js/index.js
@@ -9,3 +9,21 @@ const copyright = document.createElement('p');
 copyright.innerHTML = "Jennifer Tellez " + thisYear;
    console.log(footer); 
 footer.appendChild(copyright);
+
+const skills = [
+   "Javascript",
+   "HTML",
+   "CSS"
+];
+      
+
+const skillsSection = document.getElementById("skills")
+console.log(skillsSection)
+const skillsList = skillsSection.querySelector('ul')
+console.log(skillsList)
+
+   for (let i = 0; i < skills.length; i++) {
+       let skill = document.createElement('li')
+      skill.innerHTML = skills[i]
+      skillsList.appendChild(skill)
+   }
