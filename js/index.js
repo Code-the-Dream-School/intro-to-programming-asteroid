aa9191989ccba3969cac12dfe7ac8ec853e75076
jennyfertellez <102021487+jennyfertellez@users.noreply.github.com>
2022-05-20 16:14:15 -0700

    Added a mesage section

--- js/index.js
+++ js/index.js
@@ -27,3 +27,31 @@ console.log(skillsList)
       skill.innerHTML = skills[i]
       skillsList.appendChild(skill)
    }
+
+   const messageForm = document.getElementsByName("leave_message")[0]
+         console.log(messageForm)
+      messageForm.addEventListener('submit', (event) => {
+      
+         event.preventDefault()
+         
+         const formName = event.target.name
+         const formEmail = event.target.email;
+         const formTextarea = event.target.message
+         console.log(formName.value, formEmail.value, formTextarea.value)
+
+         const messageSection = document.getElementById('messages')
+         const messageList = messageSection.querySelector("ul")
+         const newMessage = document.createElement('li')
+         newMessage.innerHTML = '<a href="mailto:'+formEmail.value+'" target="_blank">'+formName.value+'</a><span> wrote: '+formTextarea.value+ '</span>'
+        messageList.appendChild(newMessage)
+
+        const removeButton = document.createElement('button')
+        removeButton.textContent="Remove"
+        removeButton.type = "button"
+        removeButton.addEventListener('click', (e) => {
+           const entry = removeButton.parentNode
+           entry.remove()
+        })
+        newMessage.appendChild(removeButton)
+        messageForm.reset()
+      });
